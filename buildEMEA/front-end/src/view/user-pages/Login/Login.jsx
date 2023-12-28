60e841989e80a450255cab92bf12fd4d6a9cc064
Sibna-sherin <sibna001@gmail.com>
2023-12-28 22:07:26 +0530

    #sibna login pg yet to complete

--- buildEMEA/front-end/src/view/user-pages/Login/Login.jsx
+++ buildEMEA/front-end/src/view/user-pages/Login/Login.jsx
@@ -3,13 +3,29 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 
+import '../../../styles/User-css/login.css';
+
+
+
 function Login() {
   return (
-    <div>Login
+    <><div className='box'>
+      <div>
+      <h1 className='head'>SIGN IN</h1>
+      <h6 className='caption' >Log in with your credentials</h6>
 
-        {/* <Input type="text" placeholder="Username" /> */}
+      <form className='form'>
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" /><br></br>
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" /><br></br>
+        <button type="submit">Sign In</button>
+      </form>
+
+
+
     </div>
-  )
+      </div></>
+  )
 }
-
 export default Login
